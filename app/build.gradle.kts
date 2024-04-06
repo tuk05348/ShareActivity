c809e228081efdf6a17fb83933c8b5b7d2c7f798
Niaz Baharudeen <tuk05348@temple.edu>
2024-04-06 17:22:18 -0400

    Forgot to commit changes, used intent framework to share text used to other apps and a toast for an error if no text entered.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "edu.temple.funwithintents"
-        minSdk = 34
+        minSdk = 33
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
